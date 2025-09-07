e7756f6036fb608410210c0b9db2407cb647855e
masud-apu <amasudpervez@gmail.com>
2025-09-08 04:04:42 +0600

    Add Meta Pixel tracking code to enhance analytics and user insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,7 +87,23 @@ export default function RootLayout({
       <head>
         <OrganizationStructuredData />
         <WebsiteStructuredData />
-        
+        <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1214423220455445');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1214423220455445&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
       </head>
       <body
         className={`${raleway.variable} ${amatic.variable} ${notoSansBengali.variable} font-sans antialiased`}
