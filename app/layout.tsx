da04616e08c49f72752af48f7974696ad36d1b73
masud-apu <amasudpervez@gmail.com>
2025-09-08 04:01:13 +0600

    Remove Meta Pixel tracking code to streamline layout and improve performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from 'next/script'
 import { Raleway, Amatic_SC, Noto_Sans_Bengali } from "next/font/google";
 import { ConditionalLayout } from '@/components/conditional-layout'
 import { GlobalOverlays } from '@/components/global-overlays'
@@ -88,35 +87,11 @@ export default function RootLayout({
       <head>
         <OrganizationStructuredData />
         <WebsiteStructuredData />
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1214423220455445');
-fbq('track', 'PageView');
-          `}
-        </Script>
-        {/* End Meta Pixel Code */}
+        
       </head>
       <body
         className={`${raleway.variable} ${amatic.variable} ${notoSansBengali.variable} font-sans antialiased`}
       >
-        {/* Meta Pixel (noscript) */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1214423220455445&ev=PageView&noscript=1"
-          />
-        </noscript>
         <ConditionalLayout>
           {children}
   </ConditionalLayout>
