bc4b9037f22484884a3800ca7cb504f1bb3ce7fc
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2025-09-12 22:40:01 +0600

    may be this time!

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -1,38 +1,64 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const PIXEL_ID = "754735414125478";
+
+declare global {
+  interface Window {
+    fbq: any;
+    _fbq: any;
+  }
+}
 
 export function FacebookPixel() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((module) => module.default)
-      .then((ReactPixel) => {
-        const options = {
-          autoConfig: true,
-          debug: false,
-        };
-
-        ReactPixel.init(PIXEL_ID, undefined, options);
-        ReactPixel.pageView();
-      });
+    if (typeof window !== "undefined" && window.fbq) {
+      window.fbq("track", "PageView");
+    }
   }, [pathname, searchParams]);
 
-  return null;
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
 }
 
 export function trackEvent(event: string, data?: Record<string, unknown>) {
-  if (typeof window !== "undefined") {
-    import("react-facebook-pixel")
-      .then((module) => module.default)
-      .then((ReactPixel) => {
-        ReactPixel.track(event, data);
-      });
+  if (typeof window !== "undefined" && window.fbq) {
+    window.fbq("track", event, data);
   }
 }
 
@@ -40,12 +66,7 @@ export function trackCustomEvent(
   event: string,
   data?: Record<string, unknown>,
 ) {
-  if (typeof window !== "undefined") {
-    import("react-facebook-pixel")
-      .then((module) => module.default)
-      .then((ReactPixel) => {
-        ReactPixel.trackCustom(event, data);
-      });
+  if (typeof window !== "undefined" && window.fbq) {
+    window.fbq("trackCustom", event, data);
   }
 }
-
