33f7f9efea90fa6613a0374255f564b885f8f8a2
masud-apu <amasudpervez@gmail.com>
2026-01-20 00:20:56 +0600

    Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,6 +120,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PVKD4FLM');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Performance preconnects for critical third-parties */}
         <link rel="preconnect" href="https://res.cloudinary.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
@@ -136,6 +147,16 @@ export default function RootLayout({
       <body
         className={`${nunito.variable} ${amatic.variable} ${notoSansBengali.variable} font-sans antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PVKD4FLM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <AuthProvider>
           <PHProvider>
             {/* Wrap components using navigation hooks in Suspense */}
